7ee9d51c4fa64df1259ecacf300ee368e7c3a7fd
Margarida Ramos <margaridaramos.92@gmail.com>
2024-04-06 19:41:55 +0200

    Migrate from iText to PdfPig due to licensing models

--- codingChallengeCapzlog/Program.cs
+++ codingChallengeCapzlog/Program.cs
@@ -1,12 +1,9 @@
-﻿using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas.Parser;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using System.Globalization;
-using System;
-using System.Linq;
-
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+ 
 namespace codingChallengeCapzlog
 {
     public partial class Program
@@ -16,102 +13,106 @@ namespace codingChallengeCapzlog
             string result = ProcessFile(args);
             Console.WriteLine(result);
         }
-
-        public static string ProcessFile(string[] args) {
+ 
+        public static string ProcessFile(string[] args)
+        {
             if (args.Length != 1)
             {
                 return "Usage: codingChallengeCapzlog <path_to_pdf>";
             }
-
+ 
             string pdfPath = args[0];
             string OFP_IDENTIFIER = "E-Jet Operational Flight Plan";
             string CB_IDENTIFIER = "Observer and Jumpseat Assignments";
-
+ 
             try
             {
                 Dictionary<string, Dictionary<string, object>> allFlightsData =
                     new Dictionary<string, Dictionary<string, object>>();
-
-                using (PdfReader pdfReader = new PdfReader(pdfPath))
+ 
+                using (var pdfDocument = PdfDocument.Open(pdfPath))
                 {
-                    using (PdfDocument pdfDocument = new PdfDocument(pdfReader))
+ 
+                    foreach (var page in pdfDocument.GetPages())
                     {
-                        int numPages = pdfDocument.GetNumberOfPages();
-
-                        for (int i = 1; i <= numPages; i++)
+                        var pageContent = ContentOrderTextExtractor.GetText(page);
+ 
+                        if (pageContent.Contains(OFP_IDENTIFIER))
                         {
-                            string pageContent = ExtractTextFromPage(pdfDocument, i);
-
-                            if (pageContent.Contains(OFP_IDENTIFIER))
+                            OFPFlightData flightData = new OFPFlightData();
+                            foreach (Field field in OFPFields)
                             {
-                                OFPFlightData flightData = new OFPFlightData();
-                                foreach (Field field in OFPFields)
+                                Match match = Regex.Match(pageContent, field.MatchingRegex);
+                                if (match.Success)
                                 {
-                                    Match match = Regex.Match(pageContent, field.MatchingRegex);
-                                    if (match.Success)
+                                    if (field.FieldName == "GainLoss")
                                     {
-                                        if (field.FieldName == "GainLoss") {
-                                            flightData.SetProperty(field.FieldName, FormaGainLossData(match.Groups[1].Value));
-                                        } else {
-                                            flightData.SetProperty(field.FieldName, match.Groups[1].Value);
-                                        }
+                                        flightData.SetProperty(field.FieldName, FormaGainLossData(match.Groups[1].Value));
                                     }
                                     else
                                     {
-                                        flightData.SetProperty(field.FieldName, "None");
+                                        flightData.SetProperty(field.FieldName, match.Groups[1].Value);
                                     }
                                 }
-
-                                string standardizedDate =
-                                    DateTime.ParseExact(flightData.Date, "ddMMMyy", CultureInfo.InvariantCulture)
-                                    .ToString("ddMMMyyyy").ToUpper();
-
-                                string uniqueIdentifier = (
-                                    standardizedDate
-                                    + flightData.FlightNumber
-                                    + flightData.ATC
-                                    + flightData.AircraftRegistration
-                                ).ToUpper();
-
-                                if (!allFlightsData.ContainsKey(uniqueIdentifier))
+                                else
                                 {
-                                    // If it doesn't exist, initialize it with an empty dictionary
-                                    allFlightsData[uniqueIdentifier] = new Dictionary<string, object>();
+                                    flightData.SetProperty(field.FieldName, "None");
                                 }
-
-                                allFlightsData[uniqueIdentifier]["OFP"] = flightData;
                             }
-                            if (pageContent.Contains(CB_IDENTIFIER))
+ 
+                            string standardizedDate =
+                                DateTime.ParseExact(flightData.Date, "ddMMMyy", CultureInfo.InvariantCulture)
+                                .ToString("ddMMMyyyy").ToUpper();
+ 
+                            string uniqueIdentifier = (
+                                standardizedDate
+                                + flightData.FlightNumber
+                                + flightData.ATC
+                                + flightData.AircraftRegistration
+                            ).ToUpper();
+ 
+                            if (!allFlightsData.ContainsKey(uniqueIdentifier))
                             {
-                                CBFlightData flightData = new CBFlightData();
-                                foreach (Field field in CBFields)
-                                {
-                                    Match match = Regex.Match(pageContent, field.MatchingRegex);
-                                    if (match.Success)
-                                    {
-                                        if (field.FieldName == "CrewAndFunctions") {
-                                            flightData.SetProperty(field.FieldName, FormatCrewData(match.Groups[1].Value));
-                                        } else {
-                                            flightData.SetProperty(field.FieldName, match.Groups[1].Value);
-                                        }
-                                    }
-                                    else
-                                    {
-                                        flightData.SetProperty(field.FieldName, "None");
-                                    }
-                                }
-
-                                string uniqueIdentifier = flightData.FlightInfo
-                                    .Replace(" ", "")
-                                    .Replace("\n", "")
-                                    .Replace(".", "")
-                                    .ToUpper();
-                                allFlightsData[uniqueIdentifier]["CB"] = flightData;
+                                // If it doesn't exist, initialize it with an empty dictionary
+                                allFlightsData[uniqueIdentifier] = new Dictionary<string, object>();
                             }
+ 
+                            allFlightsData[uniqueIdentifier]["OFP"] = flightData;
                         }
-
-                        return JsonConvert.SerializeObject(allFlightsData, Formatting.Indented);
+                        if (pageContent.Contains(CB_IDENTIFIER))
+                        {
+                            CBFlightData flightData = new CBFlightData();
+                            foreach (Field field in CBFields)
+                            {
+                                Match match = Regex.Match(pageContent, field.MatchingRegex);
+                                if (match.Success)
+                                {
+                                    if (field.FieldName == "CrewAndFunctions")
+                                    {
+                                        flightData.SetProperty(field.FieldName, FormatCrewData(match.Groups[1].Value));
+                                    }
+                                    else
+                                    {
+                                        flightData.SetProperty(field.FieldName, match.Groups[1].Value);
+                                    }
+                                }
+                                else
+                                {
+                                    flightData.SetProperty(field.FieldName, "None");
+                                }
+                            }
+ 
+                            string uniqueIdentifier = flightData.FlightInfo
+                                .Replace(" ", "")
+                                .Replace("\n", "")
+                                .Replace(".", "")
+                                .ToUpper();
+ 
+                            allFlightsData[uniqueIdentifier]["CB"] = flightData;
+                        }
                     }
+ 
+                    return JsonConvert.SerializeObject(allFlightsData, Formatting.Indented);
                 }
             }
             catch (Exception ex)
@@ -119,16 +120,7 @@ namespace codingChallengeCapzlog
                 return $"Error: {ex.Message}";
             }
         }
-
-        static string ExtractTextFromPage(PdfDocument pdfDocument, int pageNumber)
-        {
-            PdfPage page = pdfDocument.GetPage(pageNumber);
-            var strategy = new SimpleTextExtractionStrategy();
-            PdfCanvasProcessor parser = new PdfCanvasProcessor(strategy);
-            parser.ProcessPageContent(page);
-            return strategy.GetResultantText();
-        }
-
+ 
         static string FormatCrewData(string input)
         {
             string[] ROLES = {
@@ -137,7 +129,7 @@ namespace codingChallengeCapzlog
                 "Commander",
                 "Copilot"
             };
-
+ 
             string parsedInput = input
                 .Replace("\n", " ")
                 .Trim()
@@ -153,16 +145,16 @@ namespace codingChallengeCapzlog
                     }
                     return acc.Trim() + " " + curr.Trim();
                 });
-
+ 
             string parsedInputWithoutRoles = parsedInput;
             foreach (string role in ROLES)
             {
                 parsedInputWithoutRoles = parsedInputWithoutRoles.Replace(" " + role, "\n");
             }
-
+ 
             return parsedInputWithoutRoles;
         }
-
+ 
         static string FormaGainLossData(string input)
         {
             string[] parts = input.Split(' ');
